c10a78bc07a17d03c9fea908ce03b6b2c5cc3078
polishq <arthur.bond@vtex.com.br>
2022-10-13 15:20:31 -0400

    Add canonical prop to SearchOpenGraph

--- react/SearchOpenGraph.tsx
+++ react/SearchOpenGraph.tsx
@@ -16,6 +16,7 @@ interface Props {
   meta: {
     title: string
     description: string
+    canonical?: string
   }
 }
 
@@ -23,13 +24,13 @@ function SearchOpenGraph(props: Props) {
   const hostname = canUseDOM ? window.location.hostname : global.__hostname__
   const pathname = canUseDOM ? window.location.pathname : global.__pathname__
   const url = `https://${hostname}${pathname}`
-  const { title, description } = props.meta
+  const { title, description, canonical } = props.meta
 
   // TODO: Add support for og:image property
   const metaTags: MetaTag[] = [
     { property: 'og:type', content: 'website' },
     { property: 'og:title', content: title },
-    { property: 'og:url', content: url },
+    { property: 'og:url', content: canonical ?? url },
     { property: 'og:description', content: description ?? '' },
   ]
 
